464a6b66f090a6a427b0e70ba058cfdc3e28e012
MatVaz14 <mativazquez-29@outlook.com>
2022-09-21 14:52:38 -0300

    02 Completed

--- 02.js
+++ 02.js
@@ -12,6 +12,13 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+  let palabraMayor = '';
+  for(let i=0; i < strings.length; i++){
+    if(strings[i].length > palabraMayor.length){
+      palabraMayor = strings[i];
+    }
+  }
+  return palabraMayor;
 }
 
 // No modifiques nada debajo de esta linea //
